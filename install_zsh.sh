95a5c7560da5779c33b42afc25d3e81c31462d69
Adamo <adamo.fapohunda@4science.com>
2024-10-14 21:17:12 +0200

    Fix

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Set package directory
-pkgdir="/"
-
-# Check if Zsh is installed
-if ! command -v zsh &> /dev/null; then
-    echo "Error: Zsh is not installed. Please install Zsh before running this script."
-    exit 1
-fi
-
 # Function to check the success of commands
 check_success() {
     if [ $? -ne 0 ]; then
@@ -17,12 +8,83 @@ check_success() {
     fi
 }
 
-# Install Zsh configuration files
-install -D -m644 .zshrc "${pkgdir}/home/${USER}/.zshrc"
-check_success "Installing .zshrc in /home/${USER}"
+# Function to clone a Git repository if the target directory doesn't exist
+clone_if_not_exists() {
+    local repo_url="$1"
+    local target_dir="$2"
 
-install -D -m644 rootzshrc "${pkgdir}/root/.zshrc"
-check_success "Installing root .zshrc"
+    if [ -d "$target_dir" ]; then
+        echo "Directory already exists: $target_dir, skipping clone."
+    else
+        sudo git clone "$repo_url" "$target_dir"
+        check_success "Cloning $(basename "$repo_url")"
+    fi
+}
+
+# Required programs
+required_cmds=("zsh" "curl" "make")
+missing_cmds=()
+
+# Check if required programs are installed
+for cmd in "${required_cmds[@]}"; do
+    if ! command -v "$cmd" &> /dev/null; then
+        missing_cmds+=("$cmd")
+    fi
+done
+
+# Report missing commands
+if [ ${#missing_cmds[@]} -ne 0 ]; then
+    echo "Error: The following required programs are not installed:"
+    for cmd in "${missing_cmds[@]}"; do
+        echo "- $cmd"
+    done
+    echo "Please install the missing programs before running this script."
+    exit 1
+fi
+
+# Define font URLs and their corresponding file names
+FONT_URLS=(
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf"
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf"
+    "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
+)
+
+FONT_NAMES=(
+    "MesloLGS NF Regular.ttf"
+    "MesloLGS NF Bold.ttf"
+    "MesloLGS NF Italic.ttf"
+    "MesloLGS NF Bold Italic.ttf"
+)
+
+# Prompt for installation directory
+read -p "Enter the installation directory for fonts (default: /usr/share/fonts/truetype/MesloLGS): " FONT_DIR
+FONT_DIR=${FONT_DIR:-/usr/share/fonts/truetype/MesloLGS}
+
+# Create the fonts directory if it doesn't exist
+sudo mkdir -p "$FONT_DIR"
+
+# Download each font into the specified directory
+for i in "${!FONT_URLS[@]}"; do
+    FONT_URL="${FONT_URLS[$i]}"
+    FONT_FILE="$FONT_DIR/${FONT_NAMES[$i]}"
+
+    # Check if the font file already exists
+    if [ -f "$FONT_FILE" ]; then
+        echo "Font already exists: ${FONT_NAMES[$i]}, skipping download."
+        continue
+    fi
+
+    echo "Downloading font: ${FONT_NAMES[$i]}..."
+    curl -L -o "$FONT_FILE" "$FONT_URL"
+
+    check_success "Downloading ${FONT_NAMES[$i]}"
+done
+
+# Update the font cache
+echo "Updating font cache..."
+sudo fc-cache -fv "$FONT_DIR"
+check_success "Unable to update fonts"
 
 # Create a temporary directory for cloning repositories
 temp_dir=$(mktemp -d)
@@ -32,51 +94,82 @@ check_success "Creating temporary directory"
 git clone https://github.com/Chrysostomus/manjaro-zsh-config.git "${temp_dir}/manjaro-zsh-config"
 check_success "Cloning manjaro-zsh-config"
 
+# Install Zsh configuration files
+install -v -D -m644 "${temp_dir}/manjaro-zsh-config/.zshrc" "$HOME/.zshrc"
+check_success "Installing .zshrc in $HOME"
+
+sudo install -v -D -m644 "${temp_dir}/manjaro-zsh-config/.zshrc" "/root/.zshrc"
+check_success "Installing root .zshrc"
+
 # Move necessary files from the cloned repository
-mv "${temp_dir}/manjaro-zsh-config/manjaro-zsh-prompt" /usr/share/zsh/
+sudo mv "${temp_dir}/manjaro-zsh-config/manjaro-zsh-prompt" /usr/share/zsh/
 check_success "Moving manjaro-zsh-prompt"
 
-mv "${temp_dir}/manjaro-zsh-config/p10k.zsh" /usr/share/zsh/
+sudo mv "${temp_dir}/manjaro-zsh-config/p10k.zsh" /usr/share/zsh/
 check_success "Moving p10k.zsh"
-
-# Copy p10k.zsh to ~/.p10k.zsh
-cp /usr/share/zsh/p10k.zsh "${pkgdir}/home/${USER}/.p10k.zsh"
-check_success "Copying p10k.zsh to ~/.p10k.zsh"
-
-# Clone Zsh plugins
-# Zsh autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "${pkgdir}/usr/share/zsh/plugins/zsh-autosuggestions"
-check_success "Cloning zsh-autosuggestions"
-
-# Zsh syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${temp_dir}/zsh-syntax-highlighting"
-check_success "Cloning zsh-syntax-highlighting"
-
-cd "${temp_dir}/zsh-syntax-highlighting" || exit
-make install
-check_success "Installing zsh-syntax-highlighting"
-mv /usr/local/share/zsh-syntax-highlighting "${pkgdir}/usr/share/zsh/plugins/zsh-syntax-highlighting"
-check_success "Moving zsh-syntax-highlighting to the correct directory"
-cd - || exit
-
-# Zsh completions
-git clone https://github.com/zsh-users/zsh-completions.git "${pkgdir}/usr/share/zsh/plugins/zsh-completions"
-check_success "Cloning zsh-completions"
-
-# Zsh history substring search
-git clone https://github.com/zsh-users/zsh-history-substring-search.git "${pkgdir}/usr/share/zsh/plugins/zsh-history-substring-search"
-check_success "Cloning zsh-history-substring-search"
-
-# Powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${pkgdir}/usr/share/zsh/theme-powerlevel10k"
-check_success "Cloning Powerlevel10k theme"
 
 # Cleanup: Remove cloned repositories
 rm -rf "${temp_dir}"
 check_success "Removing temporary directory with cloned repositories"
 
+# Copy p10k.zsh to ~/.p10k.zsh
+cp /usr/share/zsh/p10k.zsh "$HOME/.p10k.zsh"
+check_success "Copying p10k.zsh to $HOME/.p10k.zsh"
+
+
+# Clone Zsh plugins
+# Zsh autosuggestions
+clone_if_not_exists "https://github.com/zsh-users/zsh-autosuggestions.git" "/usr/share/zsh/plugins/zsh-autosuggestions"
+check_success "Unable to install zsh-autosuggestions"
+
+# Zsh syntax highlighting
+clone_if_not_exists "https://github.com/zsh-users/zsh-syntax-highlighting.git" "/usr/share/zsh/plugins/zsh-syntax-highlighting"
+check_success "Unable to install zsh-syntax-highlighting"
+
+# Zsh completions
+clone_if_not_exists "https://github.com/zsh-users/zsh-completions.git" "/usr/share/zsh/plugins/zsh-completions"
+check_success "Unable to install zsh-completions"
+
+# Zsh history substring search
+clone_if_not_exists "https://github.com/zsh-users/zsh-history-substring-search.git" "/usr/share/zsh/plugins/zsh-history-substring-search"
+check_success "Unable to install zsh-history-substring-search"
+
+# Powerlevel10k theme
+clone_if_not_exists "https://github.com/romkatv/powerlevel10k.git" "/usr/share/zsh-theme-powerlevel10k"
+check_success "Unable to install zsh-theme-powerlevel10k"
+
+
 # Add Powerlevel10k to .zshrc
-echo 'source /usr/share/zsh/theme-powerlevel10k/powerlevel10k.zsh-theme' >> "${pkgdir}/home/${USER}/.zshrc"
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >> "$HOME/.zshrc"
 check_success "Adding Powerlevel10k to .zshrc"
 
 echo "Zsh setup completed successfully."
+
+# Get the current shell
+current_shell=$(basename "$SHELL")
+echo "Your current default shell is: $current_shell"
+
+# Ask for confirmation to change the default shell to Zsh
+read -p "Do you want to change your default shell to Zsh? (y/n): " -n 1 -r
+echo    # Move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # Change the default shell
+    chsh -s "$(which zsh)"
+    echo "Default shell changed to Zsh. Please log out and log back in for the changes to take effect."
+else
+    echo "No changes made."
+fi
+
+# ASCII Art Warning
+cat << "EOF"
+.___                              __                 __   
+|   | _____ ______   ____________/  |______    _____/  |_ 
+|   |/     \\____ \ /  _ \_  __ \   __\__  \  /    \   __\
+|   |  Y Y  \  |_> >  <_> )  | \/|  |  / __ \|   |  \  |  
+|___|__|_|  /   __/ \____/|__|   |__| (____  /___|  /__|  
+          \/|__|                           \/     \/      
+
+After completing the installation, please select the **MesloLGS NF** font in your terminal emulator.
+Refer to the following link for instructions: 
+https://github.com/romkatv/powerlevel10k/blob/master/font.md#manual-font-installation
+EOF
